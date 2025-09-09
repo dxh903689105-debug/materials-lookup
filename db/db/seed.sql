09653a7103f31b1bdf8e6b66d987d11eaf6fd981
dxh903689105-debug <dxh903689105@gmail.com>
2025-09-09 16:19:34 +0800

    Delete db/db directory

--- db/db/seed.sql
+++
@@ -1,5 +0,0 @@
-sql
-INSERT INTO materials (name, category, family, key_properties, typical_applications)
-VALUES
-('Aluminum 6061', 'Metal', 'Aluminum Alloy', '{"density":"2.7 g/cm3", "yield_strength":"276 MPa"}', '{"applications":["Aircraft","Automotive"]}'),
-('PTFE (Teflon)', 'Polymer', 'Fluoropolymer', '{"density":"2.2 g/cm3", "melting_point":"327 °C"}', '{"applications":["Non-stick coatings","Seals"]}');
